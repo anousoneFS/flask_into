6697da371a4da52e970bd4206dc7f822d6338754
anousoneFS <sone.freestyle7898@gmail.com>
2021-01-30 20:51:01 +0700

    edit 2 file

--- index.py
+++ index.py
@@ -2,6 +2,11 @@ import  flask from Flask
 
 app = Flask(__name__)
 
+def name(first_name, last_name):
+    return first_name + last_name
 
 if __main__ = '__name__':
     print("hahaha")
+    print(name("anousone", "worlakoumman"))
+    print(name("sone", "freestyle"))
+    print(name("sone", "sonehaha"))
